4b63fff023d1d980c821c534f0f51671abb73dec
Sahal-Saleem <sahalsaleem2000@gmail.com>
2023-11-08 13:40:16 +0530

    first commit

---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+import {Movies} from '../../models/movies';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.css']
+})
+export class SliderComponent {
+  
+  @Input() sliderConfig : any;
+  @Input() movies !: Movies;
+  @Input() title !: string;
+
+}
